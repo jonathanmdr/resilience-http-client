b7b8c2372fc73c2014d026e67006624f5107e139
Jonathan Henrique Medeiros <jonathan.medeiros@picpay.com>
2024-12-08 16:01:01 -0300

    added authorization API with router function sample

---
+++ migrations/authorizations/V0001_create-table-authorizations.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS authorizations(
+    id VARCHAR(36) NOT NULL,
+    customer_id VARCHAR(36) NOT NULL,
+    amount DECIMAL(16, 2) NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    CONSTRAINT pk_authorizations PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
